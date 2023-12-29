0746ee36cdb93b3733e8049ee07f63efb792f4e1
musense <musense.ad@gmail.com>
2023-12-29 18:25:56 +0800

    form stage 1

--- src/app/components/UserIcon.tsx
+++ src/app/components/UserIcon.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { LuUser2 } from 'react-icons/lu'
+import { LuUserPlus2 } from 'react-icons/lu'
 
 type Props = {
+  type: 'default' | 'add'
   size?: number
 }
 
 const UserIcon = (props: Props) => {
-  const { size = 22 } = props
-  return <LuUser2 size={size} />
+  const { type = 'default', size = 22 } = props
+  if (type === 'default') {
+    return <LuUser2 size={size} />
+  }
+
+  if (type === 'add') {
+    return <LuUserPlus2 size={size} />
+  }
 }
 
 export default UserIcon
---
+++ src/app/register/RegisterForm.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled'
+import { useForm } from 'react-hook-form'
+import SubmitButton from '../components/SubmitButton'
+import FormInput from '../components/FormInput'
+
+const ErrorMessage = styled.div`
+  margin-bottom: 0.5rem;
+  color: red;
+  font-size: medium;
+`
+
+type Props = {}
+
+const RegisterForm = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+  console.log('🚀 ~ file: registerForm.tsx:19 ~ RegisterForm ~ errors:', errors)
+
+  return (
+    <form style={{ width: '100%' }} onSubmit={handleSubmit((data) => console.log(data))}>
+      <FormInput
+        label='姓名'
+        required={true}
+        inputProps={register('firstName', { required: true })}
+        error={errors.firstName?.toString()}
+      />
+      {errors.firstName && <ErrorMessage>必須填寫</ErrorMessage>}
+      <FormInput
+        label={'Email'}
+        required={true}
+        inputProps={register('email', { required: true })}
+        error={errors.email?.toString()}
+      />
+      {errors.email && <ErrorMessage>必須填寫</ErrorMessage>}
+      <FormInput
+        label='密碼'
+        type='password'
+        required={true}
+        inputProps={register('password', { required: true })}
+        error={errors.password?.toString()}
+      />
+      {errors.password && <ErrorMessage>必須填寫</ErrorMessage>}
+      <SubmitButton text={'立即註冊'} />
+    </form>
+  )
+}
+
+export default RegisterForm
